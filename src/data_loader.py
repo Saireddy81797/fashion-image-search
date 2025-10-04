de0380050f8a56e983f056295dfa60d6a050da1d
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2025-10-04 07:46:26 +0530

    data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,14 @@
+import os
+from PIL import Image
 
+class FashionDataset:
+    def __init__(self, folder):
+        self.folder = folder
+        self.image_paths = [os.path.join(folder, f) for f in os.listdir(folder) 
+                            if f.endswith(('.jpg', '.png', '.jpeg'))]
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        return Image.open(self.image_paths[idx]), self.image_paths[idx]
