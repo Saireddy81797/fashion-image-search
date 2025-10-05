d913894429af54b2f8d62405bd269706a8267021
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2025-10-05 08:34:40 +0530

    \diffusion_model.py

--- src/diffusion_model.py
+++ src/diffusion_model.py
@@ -1,13 +1,14 @@
-import torch
 from diffusers import StableDiffusionPipeline
+import torch
 
 class FashionDiffusion:
-    def __init__(self, model_id="runwayml/stable-diffusion-v1-5"):
+    def __init__(self):
+        model_id = "stabilityai/stable-diffusion-2-1-base"  # lighter than 2-1
         self.pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
         self.pipe = self.pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
-    def generate_variation(self, prompt, save_path="output.png"):
+    def generate_variation(self, prompt):
         image = self.pipe(prompt).images[0]
-        image.save(save_path)
-        return save_path
-
+        output_path = "generated_fashion.png"
+        image.save(output_path)
+        return output_path
